bd8401948bb88390d61b1a18ac52f5feb528176a
BayuArlert <agungbayu0725@gmail.com>
2025-12-16 10:25:06 +0700

    add db timeout

--- backend/database.py
+++ backend/database.py
@@ -15,7 +15,8 @@ engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=3600,
-    echo=False
+    echo=False,
+    connect_args={"connect_timeout": 60}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
