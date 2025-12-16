f16a0cf5558213e6ba0080ee9c8da76aaa241083
BayuArlert <agungbayu0725@gmail.com>
2025-12-16 11:11:16 +0700

    Fix import path for Vercel

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
